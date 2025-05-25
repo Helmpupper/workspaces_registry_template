f58669e9bc532c66a86c612bad9b1d22e0dc0a10
Helmpupper <121415847+Helmpupper@users.noreply.github.com>
2025-05-24 23:25:17 -0500

    update-01

--- site/next.config.js
+++ site/next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   output: 'export',
   distDir: '../public',
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Helmaccess',
+    description: 'Test Workspaces.',
     icon: '/img/logo.svg',
     listUrl: 'https://registry.kasmweb.com/',
     contactUrl: 'https://kasmweb.com/support',
